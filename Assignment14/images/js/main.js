d46dd19a0178e3ecd24943bf417dea006579e701
Stella Smith <112897475+Ste11ajames@users.noreply.github.com>
2024-05-02 17:51:08 -0600

    Add files via upload

---
+++ Assignment14/images/js/main.js
@@ -0,0 +1,11 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+ctx.moveTo(0,0);
+ctx.lineTo(200,0);
+ctx.stroke();
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Mk.gee's Album", 10, 50);
